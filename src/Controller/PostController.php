07cde5c6433e4cfe103a801f889cac6aaa8b772a
MinhGBD210272 <minhhxgbd210272@fpt.edu.vn>
2022-07-05 13:53:01 +0700

    cofigure post controller and add entity category and post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,12 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="app_post")
+     * @Route("/", name="app_post")
      */
     public function index(): Response
     {
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
         ]);
+    }
+    /**
+     * @Route("/post/10" , name="post_show")
+     */
+    public function show() {
+        return $this->render('post/show.html.twig');
     }
 }
---
+++ src/Entity/Post.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column (type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column (type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column (type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title):void
+    {
+        $this->title = $title;
+    }
+    /**
+     * @return mixed
+     */
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+    /**
+     * @return mixed
+     */
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
+
+}
