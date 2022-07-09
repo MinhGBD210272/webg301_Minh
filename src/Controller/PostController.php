3c60668379c4cd083597f7b9adea2a821663de9a
MinhGBD210272 <minhhxgbd210272@fpt.edu.vn>
2022-07-09 22:38:52 +0700

    post controller update and add pages nav

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,28 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
     /**
      * @Route("/", name="app_post")
      */
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, PaginatorInterface $paginator, Request $request): Response
     {
         $posts = $postRepository->findAll();
-
+        $posts = $paginator->paginate(
+            $postRepository->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+            5 /*limit per page*/
+        );
         return $this->render('post/index.html.twig', [
             'posts' => $posts
         ]);
     }
     /**
-     * @Route("/post/10" , name="post_show")
+     * @Route("/post/{id}" , name="post_show")
      */
-    public function show() {
-        return $this->render('post/show.html.twig');
+    public function show(Request $request, PostRepository $postRepository) {
+        $postId = $request->attributes->get('id');
+        $post = $postRepository->find($postId);
+
+        return $this->render('post/show.html.twig' , [
+            'post' => $post
+            ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,12 +13,19 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('en_US');
-        for ($i=1; $i<=20; $i++) {
-            $post = new Post();
-            $post->setTitle($faker->sentence(3));
-            $post->setContent($faker->text(500));
-            $post->setCreatedAt($faker->dateTimeBetween('- 3 months'));
-            $manager->persist($post);
+        for($i=1; $i <= 5; $i++){
+            $category = new Category();
+            $category->setTitle($faker->word);
+            $manager->persist($category);
+            // add post
+            for ($j=1; $j<=7; $j++) {
+                $post = new Post();
+                $post->setTitle($faker->sentence(3));
+                $post->setContent($faker->text(500));
+                $post->setCreatedAt($faker->dateTimeBetween('- 3 months'));
+                $post->setCategory($category);
+                $manager->persist($post);
+            }
         }
         $manager->flush();
     }
